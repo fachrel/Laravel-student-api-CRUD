ce7e1a6a264ad1bb31cd2ec553eda7620f63dc0f
Fachrel razka <keyroshine@gmail.com>
2023-03-05 06:20:06 +0700

    first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return response()->json(['data' => $students]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = Student::create($request->all());
+        $students = Student::all();
+
+        return response()->json($students);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'kelas' => 'required',
+            'nis' => 'required',
+            'alamat' => 'required',
+        ]);
+
+        $post = Student::findOrfail($id);
+        $post->update($request->all());
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Student::findOrfail($id);
+        $post->delete();
+
+        return response()->json(['message' => 'data deleted successfully']);
+    }
+}
